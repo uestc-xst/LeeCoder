697ce4b232a38cf417ef1d7ee8cce1c3ca5a8466
uestc-xst <1072152863@qq.com>
2016-03-25 09:28:29 +0800

    添加了3.longest substring without reepeating characters问题算法

---
+++ src/com/leecoder/src/LengthOfLongestSubstring.java
@@ -0,0 +1,80 @@
+package com.leecoder.src;
+
+import java.util.HashMap;
+
+import com.sun.org.apache.bcel.internal.generic.GOTO;
+import com.sun.xml.internal.ws.org.objectweb.asm.Label;
+
+import sun.org.mozilla.javascript.internal.regexp.SubString;
+
+public class LengthOfLongestSubstring {
+	
+	
+    /**
+     * 计算单个字符串s中不重复的最长子串，时间复杂度O(N*N)
+     * @param s
+     * @return	最长子串长度
+     */
+    public static int lengthOfLongestSubstring(String s) {
+    
+    	int max = 0;
+    	String substr = null;
+    	
+    	if (s==null) {
+			return 0;
+		}
+    	if (s.length()<=1) {
+			return s.length();
+		}else{
+			for(int i=0;i<s.length()-1;i++){
+				for(int j=i+1;j<s.length();j++){
+					substr = s.substring(i,j);
+					if (substr.contains(s.substring(j,j+1))) {
+						if (substr.length()>max) {
+							max = substr.length();
+						}
+						break;
+					}
+					if (j==s.length()-1) {
+						if (!substr.contains(s.substring(s.length()-1,s.length()))) {
+							substr = substr+s.substring(s.length()-1,s.length());
+						}
+						if (substr.length()>max) {
+							max = substr.length();
+						}						
+					}
+				}
+
+			}	
+		}
+		return max;
+        
+    }
+    
+    /**
+     * 计算单个字符串s中不重复的最长子串，时间复杂度O(N)
+     * @param s
+     * @return	最长子串长度
+     */
+    public static int lengthOfLongestSubstring2(String s) {
+    	if(s==null)
+    		return 0;
+    	char[] arr = s.toCharArray();
+    	int pre = 0;
+
+    	HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+
+    	for (int i = 0; i < arr.length; i++) {
+    		if (!map.containsKey(arr[i])) {
+    			map.put(arr[i], i);
+    		} else {
+    			pre = Math.max(pre, map.size());
+    			i = map.get(arr[i]);
+    			map.clear();
+    		}
+    	}
+
+    	return Math.max(pre, map.size());
+
+    }
+}
--- src/com/leecoder/src/Main.java
+++ src/com/leecoder/src/Main.java
@@ -22,8 +22,16 @@ public class Main {
 		ListNode l2 = null;
 		ListNode l2tmep = AddTwoNumbers.ConstructLink(l2, 991);		
 		
+		
+		
+		
 		ListNode restult = AddTwoNumbers.addTwoNumbers_xst(l1tmep,l2tmep);
 		System.out.println("hello");
+		
+		String ss = "abcabcd";
+		//System.out.println(ss.contains(ss.substring(0,1)));
+		int max = LengthOfLongestSubstring.lengthOfLongestSubstring2(ss);
+		System.out.println(max);
 	}
 
 }
